14a7ca5d5d58d3ae1603e68df0ec7cc26a88e9d4
Darryl Mittlestaedt <dkmitt@sbcglobal.net>
2018-04-18 14:37:11 -0500

    Setup & capture voice recognition

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,37 @@ catch(e); {
 	$('.app').hide();
 }
 
+
+// Setup for  Voice Recognition handlers that listen for changes to the status of the recognition
+
+recognition.onstart = function() {
+	instructions.text('Voice Recognition Activated! Try speaking into the microphone.');
+}
+
+recognition.onspeechend = function() {
+	instructions.text('You were quiet for to long, voice recognition disabled itself.');
+}
+
+recognition.onerror = function(event) {
+	if(event.error == 'no-speech') {
+		instructions.text('No speech was detected, please try again.');
+	};
+}
+
+
+// Next capture the recognition by using the onresult handler to save it in a global variable
+// and then display it in a text area on the index.html page 
+
+recognition.onresult = function(event) {
+
+	// The event is a SpeechRecognitionEvent Object.
+	// The var current holds all of the lines captured so far.
+	var current = result.eventIndex;
+
+	// transcript hold the last current retreived transcript.
+	var transpcript = event.results[current][0].transcript;
+
+	// adds the last current transcript to the contents of the note.
+	noteContent += transcript;
+	noteTextarea.val(noteContent);
+}
